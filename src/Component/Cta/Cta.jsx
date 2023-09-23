308f1266796054f061d4051081d534ae56b1d8b4
Alaguraj1 <130553601+Alaguraj1@users.noreply.github.com>
2023-09-23 18:07:32 +0530

    Update Cta.jsx

--- src/Component/Cta/Cta.jsx
+++ src/Component/Cta/Cta.jsx
@@ -11,9 +11,9 @@ function Cta() {
           <div className="col-md-6" style={{ paddingRight: "75px" }}>
             <h2 className="sec-title mb-15"> Call for Papers</h2>
             <p>Authors are invited to submit their original and unpublished work for the conference.All the abstracts of the accepted/presented papers will be published in the conference proceedings </p>
-            <a href="/registration" className="bisylms-btn">
+            <Link to="/registration" className="bisylms-btn">
               Know More
-            </a>
+            </Link>
           </div>
           <div className="col-md-6 reg_fee_sec" >
             <h2 className="sec-title mb-15">
@@ -24,9 +24,9 @@ function Cta() {
               <br /> For Foreigners 	         :     $ 50
             </p>
             <p>₹ 600 has to be paid as participation fee for additional members.</p>
-            <a href="/registration" className="bisylms-btn">
+            <Link to="/registration" className="bisylms-btn">
               Registration
-            </a>
+            </Link>
           </div>
         </div>
       </div>
