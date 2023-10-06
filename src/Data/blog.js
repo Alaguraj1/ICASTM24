1217af85541b32fa126afd606c6fa35cf3329e94
Alaguraj1 <alaguraj259@gmail.com>
2023-10-06 10:26:52 +0530

    remove bold

--- src/Data/blog.js
+++ src/Data/blog.js
@@ -4,55 +4,55 @@ export const blogs = [
     img: "assets/images/Venkatachalapathy.jpg",
     title: "Chief Patron",
     name:"Prof. Dr. B. Venkatachalapathy ",
-    roll:"<b>Vice-Chancellor</b>,<br/> Karpagam Academy of Higher Education."
+    roll:"Vice-Chancellor,<br/> Karpagam Academy of Higher Education."
   },
   {
     id: 2,
     img: "assets/images/Registrar-ravi.jpg",
     title: "Patron",
     name:"Prof. Dr. S. Ravi",
-    roll:"<b>Registrar</b>,<br/> Karpagam Academy of Higher Education."
+    roll:"Registrar,<br/> Karpagam Academy of Higher Education."
   },
   {
     id: 3,
     img: "assets/images/dr-parthasarathy.jpg",
     title: "Patron",
     name:"Prof. Dr. V. Parthasarathy ",
-    roll:"<b>Dean R & D and IR</b>,<br/> Karpagam Academy of Higher Education."
+    roll:"Dean R & D and IR,<br/> Karpagam Academy of Higher Education."
   },
   {
     id: 4,
     img: "assets/images/amutha.jpg",
     title: "Conference Chair",
     name:"Prof. Dr. A. Amudha",
-    roll:"<b>Dean</b>,<br/> Faculty of Engineering, Karpagam Academy of Higher Education."
+    roll:"Dean,<br/> Faculty of Engineering, Karpagam Academy of Higher Education."
   },
   {
     id: 5,
     img: "assets/images/Balaji.jpg",
     title: "Conference Chair",
     name:"Prof. Dr. N. V. Balaji",
-    roll:"<b>Dean</b>, Faculty of Arts, Science, Commerce and Management, Karpagam Academy of Higher Education."
+    roll:"Dean, Faculty of Arts, Science, Commerce and Management, Karpagam Academy of Higher Education."
   },
   {
     id: 6,
     img: "assets/images/sakthivel.jpg",
     title: "Convenor",
     name:"Dr. P. Sakthivel",
-    roll:"<b>Associate Professor –Physics</b>,<br/> Faculty of Engineering, Karpagam Academy of Higher Education."
+    roll:"Associate Professor –Physics,<br/> Faculty of Engineering, Karpagam Academy of Higher Education."
   },
   {
     id: 7,
     img: "assets/images/ravi.jpg",
     title: "Co-Convenor",
     name:"Dr. K. Ravichandran",
-    roll:"<b>Associate Professor and Head</b>,<br/> Department of Physics,<br/> A. V. V. M. Sri Pushpam College (Autonomous), Poondi."
+    roll:"Associate Professor and Head,<br/> Department of Physics,<br/> A. V. V. M. Sri Pushpam College (Autonomous), Poondi."
   },
   {
     id: 8,
     img: "assets/images/mangalaraja.jpg",
     title: "Co-Convenor",
     name:"Dr. Mangalaraja Ramalinga Viswanathan",
-    roll:"<b>Professor</b>, Faculty of Engineering and Sciences, Universidad Adolfo Ibanez,<br/> Santiago, Chile."
+    roll:"Professor , Faculty of Engineering and Sciences, Universidad Adolfo Ibanez,<br/> Santiago, Chile."
   },
 ];
