9bb5ab8ef71bdcc73391135585174a369fc45c7e
Alaguraj1 <alaguraj259@gmail.com>
2023-10-06 11:20:45 +0530

    changes

--- src/Component/Course/FeatureCourse.jsx
+++ src/Component/Course/FeatureCourse.jsx
@@ -20,27 +20,6 @@ function FeatureCourse() {
             <FeatureCourseCard key={cours.id} course={cours} />
           ))}
         </div>
-        {/* <div className='row'>
-          <div className="col-lg-4 col-md-6">
-            <div className="feature-course-item-3">
-              <div className="fcf-thumb">
-                <img src="assets/images/Praseetha.jpg" alt="" />
-                <Link className="enroll" to="/chief-guests">
-                  Enroll Now
-                </Link>
-              </div>
-              <div className="fci-details">
-                <h4>
-                  Dr. P. K. Praseetha
-                </h4>
-                <p style={{ fontSize: "14px" }}><b>Professor and Head,  Department of Nanotechnology,</b><br/> Noorul Islam Centre for Higher Education, Nagercoil, India.</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-8 col-md-6">
-            <img src="assets/images/kahe.jpg" alt="" className="kahe-img" />
-          </div>
-        </div> */}
       </div>
     </section>
   );
--- src/Component/DownloadSec/DownloadSec.jsx
+++ src/Component/DownloadSec/DownloadSec.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 
 const DownloadSec = () => {
-  return (
-    <section
-    className="download-section"
-  >
-    <div className="container">
-      <div className='row'>
-        <div className='col-md-6'>
-          <h3>Downloads</h3>
-          <ul>
-            <li>Brochure</li>
-            <li>Abstract Template</li>
-          </ul>
-        </div>
-        <div className='col-md-6'>
-        </div>
-      </div>
+    return (
+        <section
+            className="download-section"
+        >
+            <div className="container">
+                <div className='row Download-main'>
+                    <div className='col-md-6'>
+                    <h3 className=' Download-Head'>Downloads</h3>
+                        <ul style={{ paddingLeft: "0px" }}>
+                            <div className="ci-item with-bg Download-List">
+                                <h5>
+                                    <i class="fa fa-download" aria-hidden="true"></i>
+                                    <a href="assets/images/abstract_template.docx" target="_blank" rel="noopener noreferrer" >Brochure</a>
+                                </h5>
+                            </div>
+                            <div className="ci-item with-bg ">
+                                <h5>
+                                    <i class="fa fa-download" aria-hidden="true"></i>
+                                    <a href="assets/images/abstract_template.docx" target="_blank" rel="noopener noreferrer" >Abstract Template</a>
+                                </h5>
+                            </div>
+                        </ul>
+                    </div>
+                    <div className='col-md-6'>
+                        <img src="/assets/images/nanomaterials.jpg" alt="" className="nanomaterial" />
+                    </div>
+                </div>
 
-    </div>
-  </section>
-  )
+            </div>
+        </section>
+    )
 }
 
 export default DownloadSec
