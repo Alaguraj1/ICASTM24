b244c2dd3ef5e871acf0d162c37a55e5df12a167
Alaguraj1 <alaguraj259@gmail.com>
2023-12-21 12:34:22 +0530

    changes

--- src/Component/Features/FeatureHome3.jsx
+++ src/Component/Features/FeatureHome3.jsx
@@ -5,22 +5,22 @@ function FeatureHome3() {
     <section className="feature-section">
       <div className="container">
         <div className="row">
-          <div className="col-md-3 col-lg-6">
+          <div className="col-md-6 col-lg-6 col-xl-3">
             <div className="feature-item">
               <img src="assets/images/kahe-logo.jpg" alt="logo-1" />
             </div>
           </div>
-          <div className="col-md-3  col-lg-6">
+          <div className="col-md-6  col-lg-6 col-xl-3">
             <div className="feature-item">
               <img src="assets/images/s2.jpg" alt="logo-2" />
             </div>
           </div>
-          <div className="col-md-3  col-lg-6">
+          <div className="col-md-6  col-lg-6 col-xl-3">
             <div className="feature-item">
               <img src="assets/images/header-logo-3.jpg" alt="logo-3" />
             </div>
           </div>
-          <div className="col-md-3  col-lg-6">
+          <div className="col-md-6  col-lg-6 col-xl-3">
             <div className="feature-item">
               <p className="feature-item-para">Publication Partner :</p>
               <img src="assets/images/header-logo-4.jpg" alt="logo-4" />
